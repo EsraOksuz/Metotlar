65e37f5fb5b1de6470249580523c745346182cc5
esraoksuz <esraoksuz@posta.mu.edu.tr>
2021-01-17 21:57:33 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyat = 15;
+            urun1.Aciklama = "Amasya sulusu";
+
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyat = 80;
+            urun2.Aciklama = "Diyarbakır sulusu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+            
+            //type-safe
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyat);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("------------");
+
+            }
+
+            Console.WriteLine("-----------Metotlar---------");
+
+            SepetManager sepetManager = new SepetManager();
+
+            sepetManager.Ekle(urun1);   //encapsulation
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12, 20);
+            sepetManager.Ekle2("Elma", "Yeşil Elma", 10, 60);
+            sepetManager.Ekle2("Kavun", "Sarı Kavun", 18, 3);
+
+            Console.ReadLine();
+        
+        }
+    }
+}
---
+++ Metotlar/Urun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class Urun
+    {
+        //property- özellik
+
+        public int Id { get; set; }
+
+        public string Adi { get; set; }
+
+        public double Fiyat { get; set; }
+
+        public string Aciklama { get; set; }
+
+        public int StokAdeti { get; set; }
+    }
+}
